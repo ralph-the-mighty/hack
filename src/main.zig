62689605bac7cecdc6e8f206f66c0a64069123c4
Josh <jtdonahue360@gmail.com>
2022-08-19 21:59:58 -0400

    decode single instruction string into binary format

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,16 @@
 const std = @import("std");
 
+pub fn decode_instruction(instruction_string: []const u8) u16 {
+    var instr: u16 = 0;
+    var i: usize = 0;
+    while (i < 16) : (i += 1) {
+        if (instruction_string[i] == '1') {
+            instr |= (@intCast(u16, 1) << (15 - @intCast(u4, i)));
+        }
+    }
+    return instr;
+}
+
 pub fn main() anyerror!void {
     // Note that info level log messages are by default printed only in Debug
     // and ReleaseSafe build modes.
@@ -17,4 +28,8 @@ pub fn main() anyerror!void {
 
 test "basic test" {
     try std.testing.expectEqual(10, 3 + 7);
+}
+
+test "parse instruction" {
+    try std.testing.expectEqual(decode_instruction("0101010101010101"), @intCast(u16, 0b0101_0101_0101_0101));
 }
